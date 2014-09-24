cc56bcfa6844f0b0765355528859bce954801f61
sarahkershaw <sarahlkershaw@Sarah-Kershaws-MacBook-Pro-2.local>
2014-09-24 14:15:57 -0400

    Initial JS attempt
    Still having trouble with final line of code (assigning colour to
    switch on a light). Want to test it in a browser to see if I can make
    more sense of the console then.

--- GA_Projects_3_Traffic_Light/JS/applications.js
+++ GA_Projects_3_Traffic_Light/JS/applications.js
@@ -1,1 +1,13 @@
-alert("why hello there")
+function buttonClick(color) {
+  
+  var orange=document.getElementById("orangeLight");
+  var green=document.getElementById("goLight");
+  var red=document.getElementById("stopLight");
+  
+  orange.style.backgroundColor="#3d3d3d";
+  green.style.backgroundColor="#3d3d3d";
+  red.style.backgroundColor="#3d3d3d";
+  
+  var light=document.getElementById(color);
+  light.style.backgroundColor = color;
+}
