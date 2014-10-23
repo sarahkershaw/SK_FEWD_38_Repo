ecba6d0621208a702acb18317273522ab7a49962
sarahkershaw <sarahlkershaw@Sarah-Kershaws-MacBook-Pro-2.local>
2014-10-23 16:00:09 -0400

    Final Project updates

--- GA_Projects_Final_Project_/js/main.js
+++ GA_Projects_Final_Project_/js/main.js
@@ -3,7 +3,6 @@ var imageOutcomes = [ "images/image_0_nocollection.jpg", "images/image_1_recycli
 
 //calling API function //
 
-function get311Data(){
 $.ajax({
 method:"GET",
 url:("https:api.cityofnewyork.us/311/v1/municipalservices?app_id=0114b4dd&app_key=ee16c5f7409497e6b342df2fd061df24?details=&status"),
@@ -20,28 +19,30 @@ error:function(error){
 //functions to display image depending on API result and time and date of week //
 
 function displayImage(imageOptions){
-	document.getElementById('apiResultImage');
-	img.src="images/image_0_nocollection.jpg";
+	var img=document.getElementById('apiResultImage');
+	img.setAttribute('src',images[imageOutcomes]);
+	currentImage=imageOutcomes[0];
 	status="no collection today, go outside, be free, explore the open city and all her possibilities"
 	
-	if (outcome!="on schedule")
-	document.getElementById('apiImageResult');
-    src="images/image_3_collectionexception.jpg"
-    status="Ah! Polar Vortex or that beast from Cloverfield is back in town. No collection today!";
+	if (outcome!="on schedule"){
+	var img=document.getElementById('apiResultImage');
+    img.setAttribute('src',images[imageOutcomes]);
+    currentImage=imageOptions;
+    status="Ah! Something has gone wrong. Polar Vortex or that beast from Cloverfield is back in town. No collection today!";
 };
 
 	else if (outcome=="on schedule" day== "Sunday" or "Tuesday" AND
 	time <5 >24) {
-	document.getElementById('apiResultImage') 
-	src=images/image_2_regulartrashblackwhitebags.jpg">
-	status="Normal trash collection, black and white bags";
+	var img=document.getElementById('apiResultImage') 
+	img.setAttribute('src',images[imageOutcomes]);
+	status="Normal trash collection, black and white bags for collection tomorrow morning.";
 };
 	
 	else (outcome=="on schedule" day=="Thursday" AND
 	time= <5 >24) ) {
-	document.getElementById('apiResultImage')
-	src="images/image_1_recyclingcollection.jpg">;
-	status="Thundercats are go! Recycling bags, (neatly) folded cardboard and regular black or white rubbish bags. It is a trash jackpot!"
+	var img=document.getElementById('apiResultImage')
+	img.setAttribute('src'=images[imageOutcomes]);
+	status="Thundercats are go! Recycling bags, (neatly) folded cardboard and regular black or white rubbish bags. It is a trash jackpot! All out for collection tomorrow morning"
 }
 	 document.getElementById("apiResult").innerHTML=status;
 });
