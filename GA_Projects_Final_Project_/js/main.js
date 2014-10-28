a9ede2dbc277d6e71ffa3691d64c72f0d2dcf74b
sarahkershaw <sarahlkershaw@Sarah-Kershaws-MacBook-Pro-2.local>
2014-10-28 18:58:35 -0400

    Final Project
    Upload! Still potential for CSS tweaking

--- GA_Projects_Final_Project_/js/main.js
+++ GA_Projects_Final_Project_/js/main.js
@@ -1,48 +1,58 @@
-alert("Hudson rules the house");
-var imageOutcomes = [ "images/image_0_nocollection.jpg", "images/image_1_recyclingcollection.jpg", "images/image_2_regulartrashblackwhitebags.jpg","images/image_3_collectionexception.jpg"];
+var imageOutcomes = [ "images/image_0_nocollection.jpg", "images/image_1_recyclingcollection.jpg", "images/image_2_regularcollectionblackwhitebags.jpg","images/image_3_collectionexception.jpg"];
+var outcome;
 
 //calling API function //
-
-$.ajax({
-method:"GET",
-url:("https:api.cityofnewyork.us/311/v1/municipalservices?app_id=0114b4dd&app_key=ee16c5f7409497e6b342df2fd061df24?details=&status"),
-success:function(data){
-$("$outcome").html(data.outcome);
+function getStatus(){
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8000/api",
+		dataType: "jsonp",
+		success: function(data){
+			console.log(data);
+			displayImage(data);
+		},
+		error: function(error){
+			alert ("something somewhere has gone wrong. If in doubt, revert to normal schedule");
+			console.log(error);
+		}
+	});
 }
-}
+// console.log(outcome);
+$( document ).ready(function() {
+    getStatus();
 });
-
-error:function(error){
- alert ("something somewhere has gone wrong. If in doubt, revert to normal schedule");
-};
 
 //functions to display image depending on API result and time and date of week //
 
-function displayImage(imageOptions){
-	var img=document.getElementById('apiResultImage');
-	img.setAttribute('src',images[imageOutcomes]);
-	currentImage=imageOutcomes[0];
-	status="no collection today, go outside, be free, explore the open city and all her possibilities"
-	
-	if (outcome!="on schedule"){
-	var img=document.getElementById('apiResultImage');
-    img.setAttribute('src',images[imageOutcomes]);
-    currentImage=imageOptions;
-    status="Ah! Something has gone wrong. Polar Vortex or that beast from Cloverfield is back in town. No collection today!";
-};
+function displayImage(data){
+	var status = data["items"][0]["items"][1]["status"];
+	var d = new Date();
+	var day = d.getDay();
+	var hour = d.getHours();
+	//hour = 20;
+	//day = 4;
+	//status="ON SCHEDULE";
 
-	else if (outcome=="on schedule" day== "Sunday" or "Tuesday" AND
-	time <5 >24) {
-	var img=document.getElementById('apiResultImage') 
-	img.setAttribute('src',images[imageOutcomes]);
-	status="Normal trash collection, black and white bags for collection tomorrow morning.";
-};
+	var img=document.getElementById('apiResultImage');
+	currentImage=imageOutcomes[0];
+	img.setAttribute('src', currentImage); 
+	statusMessage="No collection today, go outside, be free, explore the open city and all her possibilities"
 	
-	else (outcome=="on schedule" day=="Thursday" AND
-	time= <5 >24) ) {
-	var img=document.getElementById('apiResultImage')
-	img.setAttribute('src'=images[imageOutcomes]);
-	status="Thundercats are go! Recycling bags, (neatly) folded cardboard and regular black or white rubbish bags. It is a trash jackpot! All out for collection tomorrow morning"
-}
-	 document.getElementById("apiResult").innerHTML=status;
-});
+	if (status != "ON SCHEDULE"){
+		var img = document.getElementById('apiResultImage');
+	    currentImage = imageOutcomes[3];
+	    img.setAttribute('src',currentImage);
+	    statusMessage = "Ah! Something somewhere has gone wrong. It's the Polar Vortex or that beast from Cloverfield is back in town. No collection today.";
+	} else if ((day == 0 || day == 2) && (hour >= 17) && (hour <= 23)) {
+		var img=document.getElementById('apiResultImage') 
+		currentImage=imageOutcomes[2];
+		img.setAttribute('src', currentImage);
+		statusMessage="Normal trash collection, ready those black and white bags for collection tomorrow morning.";
+	} else if ((day == 4) && (hour >= 17) && (hour <=23)) {
+		var img=document.getElementById('apiResultImage')
+		currentImage=imageOutcomes[1];
+		img.setAttribute('src', currentImage);
+		statusMessage = "Thundercats are go! Prepare your recycling bags, (neatly) folded cardboard and regular black or white rubbish bags. Put them all out for collection tomorrow morning"
+	}
+	 document.getElementById("apiResult").innerHTML=statusMessage;
+};
